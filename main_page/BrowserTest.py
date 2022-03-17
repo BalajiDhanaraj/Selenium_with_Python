7e7977fa33c5126fc2ae284ce6b8930261576575
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-03-17 10:41:53 +0530

    update

--- main_page/BrowserTest.py
+++ main_page/BrowserTest.py
@@ -1,9 +1,19 @@
 from selenium import webdriver
 
+driver = webdriver.Chrome(executable_path="/Volumes/Macintosh HD/For Mac/python project/Browserdrivers/chromedriver")
 
+driver.get("http://way2automation.com")
 
+driver.maximize_window()
 
+title = driver.title
 
+print(title)
+
+assert "Selenium" in title
+
+driver.close()
+driver.quit()
 
 
 
