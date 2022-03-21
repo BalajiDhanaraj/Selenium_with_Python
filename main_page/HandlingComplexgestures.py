3dc818fe020d5c10ca1c5faf7278b6f85d33ef62
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-03-21 11:02:05 +0530

    update

--- main_page/HandlingComplexgestures.py
+++ main_page/HandlingComplexgestures.py
@@ -8,29 +8,41 @@ import time
 driver = webdriver.Chrome("/Volumes/Macintosh HD/For Mac/python project/Browserdrivers/chromedriver")
 
 
-### this is the check is element present
-
+""" ### Mouse Handover
 driver.get("https://www.way2automation.com/")
-
 driver.implicitly_wait(50)
 
-### Mouse Handover
 
 menu = driver.find_element(by=By.XPATH,value="//li[@id='menu-item-27580']//span[contains(text(),'All Courses')]")
-
 time.sleep(2)
-
 action = ActionChains(driver)
 action.move_to_element(menu).perform()
 time.sleep(2)
 driver.find_element(by=By.XPATH,value="//li[@id='menu-item-27595']//span[contains(text(),'Robot Framework')]")
 
-
 driver.quit()
-# driver.close()
+# driver.close() """
 
 
 
+### Moving Sliders
+
+driver.get("http://demo.automationtesting.in/Slider.html")
+driver.implicitly_wait(60)
+
+slider = driver.find_element(by=By.XPATH,value="//a[@class='ui-slider-handle ui-state-default ui-corner-all']")
+   ## getting the location and size of the slider
+location = slider.location
+size = slider.size
+
+w,h = size['width'],size['height']
+print(location,size)
+print(w,h)
+
+
+ActionChains(driver).drag_and_drop_by_offset(slider,w/2,0).perform()
+driver.quit()
+driver.close()
 
 
 
