434b746ebebd2b85568848fb9111890dce8a0211
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-03-23 10:46:16 +0530

    update

---
+++ main_page/Javascript_and_Screenshot.py
@@ -0,0 +1,7 @@
+from selenium import  webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome("/Volumes/Macintosh HD/For Mac/python project/Browserdrivers/chromedriver")
