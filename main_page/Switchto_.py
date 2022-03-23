1b27e42a51760b1c33f5b7ca6cbb4ecdd3e87c7c
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-03-23 10:40:30 +0530

    update

--- main_page/Switchto_.py
+++ main_page/Switchto_.py
@@ -43,7 +43,7 @@ for frame in frames:
 driver.close()
 driver.quit()"""
 
-## Switch to popup window
+"""## Switch to popup window
 
 driver.get("http://www.way2automation.com/")
 
@@ -65,8 +65,26 @@ driver.find_element_by_id("user_email").send_keys("trainer@way2automation.com")
 
 driver.close()
 driver.quit()
+"""
+"""## Switch to another window
 
-## Switch to another window
+driver.get("http://demo.guru99.com/popup.php")
+
+driver.find_element(by=By.XPATH,value="//*[contains(@href,'popup.php')]").click()
+
+driver.switch_to.window(driver.window_handles[1])
+
+driver.find_element(by=By.NAME,value="emailid").send_keys("gaurav.3n@gmail.com")
+
+driver.find_element(by=By.NAME,value="btnLogin").click()
+driver.close()
+
+driver.switch_to.window(driver.window_handles[0])
+
+
+driver.close()
+driver.quit()"""
+
 
 
 
