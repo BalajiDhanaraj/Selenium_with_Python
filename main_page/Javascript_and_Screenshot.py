f717b820d843fd721cdec4172b7a45f968dfa1fe
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-03-24 11:18:08 +0530

    update

--- main_page/Javascript_and_Screenshot.py
+++ main_page/Javascript_and_Screenshot.py
@@ -1,8 +1,11 @@
-from selenium import  webdriver
+import element as element
+from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 import time
+
+from selenium.webdriver.remote.webelement import WebElement
 
 driver = webdriver.Chrome("/Volumes/Macintosh HD/For Mac/python project/Browserdrivers/chromedriver")
 
@@ -13,39 +16,26 @@ driver.get("https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_submit_g
 driver.implicitly_wait(50)
 time.sleep(2)
 
-def capture_screenshot(d,name):
-    file_name = "/Volumes/Macintosh HD/For Mac/python project/Selenium_with_Python/main_page/screenshot/"+name+"_"+time.asctime().replace(":","_")+".png"
-    d.save_scrreenshot(file_name)
+
+def capture_screenshot(name):
+    file_name = "/Volumes/Macintosh HD/For Mac/python project/Selenium_with_Python/main_page/screenshot/" + name + "_" + time.asctime().replace(
+        ":", "_") + ".png"
+    element.save_scrreenshot(file_name)
+
+
 driver.switch_to.frame("iframeResult")
 print("In iframe ")
 # driver.find_element(by=By.XPATH,value="//input[@id='mySubmit']").click()
 
-driver.execute_script("myFunction()")
+driver.execute_script("myFunction()").capture_screenshot("Test1")
 driver.switch_to.default_content()
 
-#
+
+# #
 # elem = driver.find_element(by=By.ID,value="mySubmit")
 # driver.execute_script("arguments[0].style.border='3px solid red' ",elem)
 
-capture_screenshot(driver,"Test1")
 
 print("Out of iframes ")
 driver.close()
 driver.quit()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
