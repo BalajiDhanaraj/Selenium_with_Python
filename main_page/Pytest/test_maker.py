4d09367ec17908579561832024d8193b71819dbf
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-03-26 14:49:36 +0530

    update

---
+++ main_page/Pytest/test_maker.py
@@ -0,0 +1,15 @@
+import pyttest
+
+""" Using Maker """
+
+
+def test_login():
+    print("Executing login test")
+
+
+def test_user_reg():
+    print("Executing user reg test")
+
+
+def test_compose_email():
+    print("Executing compose email")
