188cd3fcb4b793c87600ccd72fe9d669b4e1d96a
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-03-26 15:06:53 +0530

    update

--- main_page/Pytest/test_maker.py
+++ main_page/Pytest/test_maker.py
@@ -1,15 +1,15 @@
-import pyttest
+import pytest
 
 """ Using Maker """
 
-
+@pytest.mark.functional
 def test_login():
-    print("Executing login test")
+    print("Executing login test",end=" ")
 
-
+@pytest.mark.regression
 def test_user_reg():
-    print("Executing user reg test")
+    print("Executing user reg test",end=" ")
 
-
+@pytest.mark.functional
 def test_compose_email():
-    print("Executing compose email")
+    print("Executing compose email",end=" ")
